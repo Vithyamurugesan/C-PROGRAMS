fbda118886666521c9529f2dab441265ca1e83fc
Vithyamurugesan <vithyabtech54@gmail.com>
2025-08-11 21:58:26 +0530

    Delete size.c

--- size.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    printf("%lu bytes\n",sizeof(int));
-    printf("%lu bytes\n",sizeof(float));
-    printf("%lu bytes\n",sizeof(double));
-    printf("%lu bytes\n",sizeof(char));
-    return 0;
-
-}
