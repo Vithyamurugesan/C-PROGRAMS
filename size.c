22d3b995092a6888435cfe9447f2204260ec8d50
Vithyamurugesan <vithyabtech54@gmail.com>
2025-08-11 21:32:56 +0530

    Add files via upload

---
+++ size.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("%lu bytes\n",sizeof(int));
+    printf("%lu bytes\n",sizeof(float));
+    printf("%lu bytes\n",sizeof(double));
+    printf("%lu bytes\n",sizeof(char));
+    return 0;
+
+}
