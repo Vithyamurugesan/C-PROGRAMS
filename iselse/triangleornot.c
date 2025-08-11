766a1dec386eab1905e48eae076b0c645dbec5c1
Vithyamurugesan <vithyabtech54@gmail.com>
2025-08-11 22:09:31 +0530

    Add files via upload

---
+++ iselse/triangleornot.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int angle1,angle2,angle3;
+    scanf("%d %d %d",&angle1,&angle2,&angle3);
+    if(angle1+angle2+angle3==180 && angle1!=0 && angle2!=0 && angle3!=0){
+        printf("Triangle is valid");
+    }
+    else{
+        printf("Triangle is not valid");
+    }
+    return 0;
+
+}
