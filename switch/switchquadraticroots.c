09efe2f08c4fd28c8bae7d6fc2aad725195e1659
Vithyamurugesan <vithyabtech54@gmail.com>
2025-08-19 11:30:26 +0530

    Add files via upload

---
+++ switch/switchquadraticroots.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<Math.h>
+int main(){
+    int choice;
+    printf("Enter the choice\n");
+    scanf("%d",&choice);
+    printf("1.linear Equation(ax+b=0)\n");
+    printf("2.quadratic Equation(ax^2+bx+c=0)\n");
+    switch(choice){
+    case 1:{
+        float a,b;
+        printf("Enter the coeeficients a and b:\n");
+        scanf("%f %f",&a,&b);
+        if(a!=0){
+            printf("Roots:%.2f",-b/a);
+        }
+        else{
+            printf("Invalid Equation");
+        }
+        break;   
+    }
+    case 2:{
+        float a,b,c,d;
+        printf("Enter the coeeficients a,b,c:\n");
+        scanf("%f %f %f",&a,&b,&c);
+        d=b*b-4*a*c;
+        if(d>0){
+            printf("Roots:%.2f and %.2f\n",(-b+sqrt(d))/(2*a),(-b-sqrt(d))/(2*a));
+        }
+        else if(d==0){
+            printf("Roots:%.2f\n",-b/(2*a));
+        }
+        else{
+            printf("Complex Roots\n");
+        }
+        break;   
+    }
+    default:
+    printf("Invalid");
+}
+return 0;  
+}
