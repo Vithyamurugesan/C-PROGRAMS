c55f18e5bf5c8177d641e3f4e46ec49d39515853
Vithyamurugesan <vithyabtech54@gmail.com>
2025-08-26 11:36:11 +0530

    Add files via upload

---
+++ array/arrdeleteallduplicates.c
@@ -0,0 +1,22 @@
+//Unique elements
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int count;
+    for(int i=0;i<n;i++){
+         count=0;
+        for(int j=0;j<n;j++){
+            if(arr[i]==arr[j])
+            count++;
+        }
+        if(count==1){
+        printf("%d",arr[i]);
+        }
+    }
+    return 0;    
+}
