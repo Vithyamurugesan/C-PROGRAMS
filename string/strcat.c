88a4f7ac480200fa725de8fa6e166d4ae4f9ce19
Vithyamurugesan <vithyabtech54@gmail.com>
2025-08-31 15:07:52 +0530

    Add files via upload

---
+++ string/strcat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100];
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    strcat(str1, str2);   // append str2 to str1
+
+    printf("Concatenated string: %s\n", str1);
+
+    return 0;
+}
