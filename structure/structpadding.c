847337c8f356f2cecbcf7f9b54749a60243b65f4
Vithyamurugesan <vithyabtech54@gmail.com>
2025-10-11 11:47:18 +0530

    Add files via upload

---
+++ structure/structpadding.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+struct abc {
+    char a;
+    int b;
+};
+
+int main() {
+    printf("Size of struct abc = %d bytes\n", sizeof(struct abc));
+    return 0;
+}
+
+
+
+// #include <stdio.h>
+
+// struct abc {
+//     char a;
+//     int b;
+//     char c;
+// };
+
+// int main() {
+//     printf("Size of struct abc = %d bytes\n", sizeof(struct abc));
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// struct abc {
+//     char c;
+//     char a;
+//     int b;
+// };
+
+// int main() {
+//     printf("Size of struct abc = %d bytes\n", sizeof(struct abc));
+//     return 0;
+// }
+
+
+
+// #include <stdio.h>
+
+// struct abc {
+//     char a;
+//     short int b;
+//     char c;
+// };
+
+// int main() {
+//     printf("Size of struct abc = %lu bytes\n", sizeof(struct abc));
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// struct abc {
+//     char a[35];
+// };
+// int main() {
+//     printf("Size of struct abc = %d bytes\n", sizeof(struct abc));
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// struct abc {
+//     char a[35];
+//     int b;
+// };
+// int main() {
+//     printf("Size of struct abc = %d bytes\n", sizeof(struct abc));
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// struct abc {
+//     char a;
+//     int b;
+//     double c;
+// };
+// int main() {
+//     printf("Size of struct abc = %d bytes\n", sizeof(struct abc));
+//     return 0;
+// }
+
+
